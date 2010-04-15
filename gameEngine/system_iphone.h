f6e2ad28b3270044a6f9d1728cca85a9c78834eb
andreaskr <andreaskr@ac5cb62b-42d7-5041-bf6a-523320999cf5>
2010-04-15 19:12:56 +0000

    native ftp added for iphone

--- gameEngine/system_iphone.h
+++ gameEngine/system_iphone.h
@@ -1,9 +1,47 @@
-/*
- *  system_iphone.h
- *  DemosiPhoneOS
- *
- *  Created by Andreas on 2/15/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
+//
+//  system_iphone.h
+//  gameEditor
+//
+//  Created by Andreas Kindlbacher on 4/11/10.
+//  Copyright 2010 Visionware. All rights reserved.
+//
 
+#import <UIKit/UIKit.h>
+
+
+@interface system_iphone : NSObject {
+// For future use
+}
+
+@end
+
+@interface URLGetController : UIViewController <UITextFieldDelegate>
+{
+    //UITextField *               _urlText;
+    UIImageView *               _imageView;
+    NSString *                   statusLabel;
+    UIActivityIndicatorView *   _activityIndicator;
+    //UIBarButtonItem *           _getOrCancelButton;
+    
+    NSURLConnection *           _connection;
+    NSString *                  _filePath;
+    NSOutputStream *            _fileStream;
+	
+	NSString	*				urlText;
+	NSString	*				getOrCancelButton;
+	bool						done;
+}
+@property (nonatomic, retain)  NSString *					urlText;
+@property (nonatomic, retain)  UIImageView *               imageView;
+@property (nonatomic, retain)  NSString *                   statusLabel;
+@property (nonatomic, retain)  UIActivityIndicatorView *   activityIndicator;
+@property (nonatomic, retain)  NSString *           getOrCancelButton;
+
+- (IBAction)getOrCancelAction:(id)sender;
+- (id)init;
+- (NSURL *)smartURLForString:(NSString *)str;
+- (BOOL)isImageURL:(NSURL *)url;
+- (NSString *)pathForTemporaryFileWithPrefix:(NSString *)prefix;
+@end
+
+const char *FtpGet(char *FtpUrl);
